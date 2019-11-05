258c52a8c79ce7e702e67b668452f191e455c50d
unknown <matthewaustinbell@gmail.com>
2019-11-04 19:54:57 -0600

    wip

--- CandyMarket.Api/Repositories/CandyRepository.cs
+++ CandyMarket.Api/Repositories/CandyRepository.cs
@@ -1,20 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using CandyMarket.Api.DataModels;
+using System.Data.SqlClient;
 using CandyMarket.Api.Dtos;
+using Dapper;
 
 namespace CandyMarket.Api.Repositories
 {
     public class CandyRepository : ICandyRepository
     {
+        private string _connectionString = "Server=localhost;Database=CandyMarket;Trusted_Connection=True;";
+       
         public IEnumerable<Candy> GetAllCandy()
         {
-            throw new NotImplementedException();
+                    using (var db = new SqlConnection(_connectionString))
+                    {
+
+                        var candys = db.Query<Candy>("Select * From candy");
+
+                        return candys.AsList();
+                    }
         }
+            
 
         public bool AddCandy(AddCandyDto newCandy)
         {
-            throw new NotImplementedException();
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var sql =
+                    @"INSERT INTO [Candy]
+                                           ([Name]
+                                           ,[YearsOfExperience]
+                                           ,[Specialty])
+	                                 output inserted.*
+                                     VALUES
+                                           (@name
+                                           ,@yearsOfExperience
+                                           ,@specialty)";
+
+                return db.QueryFirst<Candy>(sql, newCandy);
+
+                //33:24
+            }
         }
 
         public bool EatCandy(Guid candyIdToDelete)
